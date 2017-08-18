78b3754fe08c64205c59e7b6d6dc8b223258b0c5
whd376908943 <wanghd.dlc@outlook.com>
2017-08-18 14:00:50 +0800

    添加用户权限认证

--- Manager/settings.py
+++ Manager/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+LOGIN_URL = '/login'
 
 STATIC_URL = '/static/'
 
--- osapp/views.py
+++ osapp/views.py
@@ -21,6 +21,7 @@ def login(request):
     return render(request, 'login.html')
 
 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
